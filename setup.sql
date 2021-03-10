5bd5efbc7d593f00c54e5df22780b08693df161d
Harry Volker <57865118+htr-volker@users.noreply.github.com>
2021-03-10 20:36:34 +0000

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -30,3 +30,10 @@ INSERT INTO Books (
         "J. R. R. Tolkien",
         "https://books.google.com/books/content/images/frontcover/FKziXsnqLTEC?fife=w200-h300"
 );
+INSERT INTO Books (
+        Name, Author, Image
+) VALUES (
+        "Harry Potter and the Philosopher's Stone",
+        "J.K. Rowling",
+        "https://books.google.com/books/content/images/frontcover/39iYWTb6n6cC?fife=w200-h300"
+);
